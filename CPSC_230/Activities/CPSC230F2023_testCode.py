37e27ea41ae6562932897b9d92e0c5555f18aaa6
Kyla Monique Cabrera <kylamonique.c@gmail.com>
2025-07-29 17:52:12 -0700

    Updated Folders

--- CPSC_230/Activities/CPSC230F2023_testCode.py
+++
@@ -1,7 +0,0 @@
-print(1+2)
-print(1+1)
-print(1+0)
-print("Installed and ready to go!")
-print ("Hello World")
-
-name = input("What is your name?")
--- CPSC_230/python.py
+++
@@ -1,3 +0,0 @@
-s = "I Love Python"
-
-print (s[1::2])
